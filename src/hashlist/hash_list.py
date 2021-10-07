cc99908129a8c195cc43b8fd799c6f907fa366dc
1Doomdie1 <horia.todoran@yahoo.com>
2021-10-07 10:19:50 +0100

    Code refactoring

--- src/hashlist/hash_list.py
+++ src/hashlist/hash_list.py
@@ -1,59 +1,61 @@
 
 class hash_list():
 
-	def __init__(self):
-		self.hash_list = {}
+    def __init__(self):
+        self.hash_list = {}
 
-	def add(self, *args):
-		for item in args:
-			key = self.__key__(item)
-			try:
-				self.hash_list[key] += [item]
-			except Exception:
-				self.hash_list[key] = [item]
+    def add(self, *args):
+        if args:
+            for item in args:
+                key = self._key_(item)
+                if self.hash_list.get(key):
+                    self.hash_list[key].append(item)
+                else:
+                    self.hash_list[key] = [item]
+        else:
+            raise Exception('No arguments passed.')
 
-	def get_item(self, item=None, index=None):
-		if item:
-			return self.__get_item_by_name__(item)
-		elif index:
-			return self.__get_item_by_index__(index)
+    def get_item_index(self, item):
+        key = self._key_(item)
+        if self.hash_list.get(key):
+            items_lst = self.hash_list[key]
+            if item in items_lst:
+                return (key, items_lst.index(item))
+            else:
+                raise Exception(f"'{item}' may have the same key but is not in hashlist")
+        else:
+            raise Exception(f"'{item}' not found")
 
-	def remove_item(self, item=None, index=None):
-		if item:
-			self.__remove_item_by_name__(item)
-		else:
-			self.__remove_item_by_index__(index)
+    def get_item_by_index(self, index):
+        try:
+            return self.hash_list[index[0]][index[1]]
+        except Exception:
+            raise Exception('Index out of bound')
 
-	def delete_index(self, index):
-		self.hash_list.pop(index)
+    def remove_item_by_index(self, index):
+        try:
+            self.hash_list[index[0]].pop(index[1])
+            if len(self.hash_list[index[0]]) == 0:
+                self.delete_index(index[0])
+        except Exception:
+            raise Exception('Index out of  bound.')
 
-	def indexes(self):
-		ind = [index for index in self.hash_list.keys()]
-		return ind
+    def remove_item_by_name(self, item):
+        index = self.get_item_index(item)
+        self.remove_item_by_index(index)
 
-	def __key__(self, word):
-		key = sum([ord(letter) for letter in word])//len(word)
-		return key
+    def delete_index(self, index):
+        self.hash_list.pop(index)
 
-	def __remove_item_by_name__(self, item):
-		itm = self.__get_item_by_name__(item)
-		self.__remove_item_by_index__(index=itm)
+    def indexes(self):
+        ind = [index for index in self.hash_list.keys()]
+        return ind
 
-	def __remove_item_by_index__(self, index):
-		self.hash_list[index[0]].pop(index[1])
-		if len(self.hash_list[index[0]]) == 0:
-			self.delete_index(index[0])
+    def _key_(self, word):
+        if word:
+            key = sum([ord(letter) for letter in word])//len(word)
+            return key
+        raise Exception('Argument can not be None')
 
-	def __get_item_by_index__(self, index):
-		return self.hash_list[index[0]][index[1]]
-	
-	def __get_item_by_name__(self, item):
-		key = self.__key__(item)
-		items_lst = self.hash_list[key]
-		if item in items_lst:
-			return (key, items_lst.index(item))
-		else:
-			raise Exception(f"'{item}'")
-
-	def print(self):
-		print(self.hash_list)
+    def print(self):
+        print(self.hash_list)
